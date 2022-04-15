faa4a57c8581bae585ae2319db153e966d236ffd
Farah <fn.rusli@outlook.com>
2022-04-16 07:37:55 +0800

    Add 8kyu codewars 'For_UFC_Fans_Total_Beginners_Conor_McGregor_vs_George_Saint_Pierre' solution

---
+++ codewars/JavaScript/8kyu/For_UFC_Fans_Total_Beginners_Conor_McGregor_vs_George_Saint_Pierre.js
@@ -0,0 +1,5 @@
+var quote = function(fighter) {
+  fighter = fighter.toLowerCase()
+  if (fighter === "george saint pierre") return "I am not impressed by your performance."
+  if (fighter === "conor mcgregor") return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+};
