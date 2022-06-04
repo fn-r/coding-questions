eb74fbea8587194b4e9071536571d90d77c55a3f
Farah <fn.rusli@outlook.com>
2022-06-05 06:27:02 +0800

    Add 8kyu codewars 'Holiday VI - Shark Pontoon' solution

---
+++ codewars/JavaScript/8kyu/Holiday VI - Shark Pontoon.js
@@ -0,0 +1,4 @@
+function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
+  if(dolphin) sharkSpeed /= 2
+  return pontoonDistance/youSpeed < sharkDistance/sharkSpeed ? "Alive!" : "Shark Bait!";
+}
