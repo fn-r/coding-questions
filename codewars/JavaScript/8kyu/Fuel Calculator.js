47cba1f685aaba248d3e7e8604cbf939bd356f14
Farah <fn.rusli@outlook.com>
2022-07-10 12:15:35 +0800

    Add 8kyu codewars 'Fuel Calculator' solution

---
+++ codewars/JavaScript/8kyu/Fuel Calculator.js
@@ -0,0 +1,11 @@
+function fuelPrice(litres, pricePerLitre) {
+  let discountCents = 0
+  if (litres >= 2)  discountCents = 0.05
+  if (litres >= 4)  discountCents = 0.1
+  if (litres >= 6)  discountCents = 0.15
+  if (litres >= 8)  discountCents = 0.2
+  if (litres >= 10) discountCents = 0.25
+  
+  const totalPrice = (litres * pricePerLitre) - (litres * discountCents)
+  return Number(totalPrice.toFixed(2))
+}
