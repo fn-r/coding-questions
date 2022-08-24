4673d123094c6f6cd8bf9e358f57cb1eb153309a
Farah <fn.rusli@outlook.com>
2022-08-24 08:37:34 +0800

    Edit 'Regex validate PIN code' solution

--- codewars/JavaScript/7kyu/Regex validate PIN code.js
+++ codewars/JavaScript/7kyu/Regex validate PIN code.js
@@ -1,4 +1,7 @@
-function validatePIN (pin) {
-  const l = pin.length
-  return (l === 4 || l === 6)
+function validatePIN(pin) {
+    const l = pin.length
+    const pinArr = pin.split('').filter(n => '1234567890'.includes(n))
+
+    if (pinArr.length !== l) return false
+    return (l === 4 || l === 6)
 }
