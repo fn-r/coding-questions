07b6d655ac32435742dda7e13b1dcb09f5e5cdcb
Farah <fn.rusli@outlook.com>
2022-10-20 10:12:40 +0800

    Add solutions

---
+++ codewars/Python/7kyu/Fizz Buzz.py
@@ -0,0 +1,14 @@
+def fizzbuzz(n):
+    arr = []
+    
+    for i in range(1, n + 1):
+        if (i % 3 == 0 and i % 5 == 0):
+            arr.append("FizzBuzz")
+        elif (i % 3 == 0):
+            arr.append("Fizz")
+        elif (i % 5 == 0):
+            arr.append("Buzz")
+        else:
+            arr.append(i)
+    
+    return arr
